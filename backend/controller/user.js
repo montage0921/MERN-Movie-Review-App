721399635e52f418dc875e3d551e2937ba39662a
montage0921 <garyshi0921@gmail.com>
2023-12-17 17:27:03 -0500

    add error handling for duplicate email problem

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -8,12 +8,24 @@ exports.create = async (req, res) => {
   //similar as OOP
   const newUser = new User({ name, email, password });
 
-  //save the new user to mongoDB database
-  //it's an async step
-  await newUser.save();
+  try {
+    //save the new user to mongoDB database
+    //it's an async step
+    await newUser.save();
 
-  //res.json(): send a json format response to front end
-  res.json({
-    user: newUser,
-  });
+    //res.json(): send a json format response to front end
+    res.json({
+      user: newUser,
+    });
+  } catch (err) {
+    if (err.code == 11000)
+      res.status(400).json({ error: "email already exists!" });
+    else {
+      res
+        .status(500)
+        .json({
+          error: "an error occurred, please fix it and resend the request!",
+        });
+    }
+  }
 };
--- backend/db/index.js
+++ backend/db/index.js
@@ -5,7 +5,7 @@ mongoose
     "mongodb+srv://nicaiwojiaosha:nicaiwojiaosha1.A@moviereviewapp.f9hhksl.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
-    console.log("db is connected");
+    console.log("db is connected!");
   })
   .catch((ex) => {
     console.log(`db connection is failed: ${ex}`);
