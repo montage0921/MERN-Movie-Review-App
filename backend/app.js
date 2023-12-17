2d7223d6ed127c3ac3ef0bd26699b199ab1004c0
montage0921 <garyshi0921@gmail.com>
2023-12-16 19:01:59 -0500

    add about url

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,11 @@ const userRouter = require("./routes/user");
 
 const app = express();
 
-app.use('/api/user',userRouter);
+app.use("/api/user", userRouter);
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Hello I am from backend!</h1>");
+});
 
 app.listen(1108, () => {
   console.log("the port is listening on port 1108");
