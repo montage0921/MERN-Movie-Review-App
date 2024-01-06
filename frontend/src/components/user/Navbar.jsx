45e9ebf24797a3a03bce2c5a7d0ee10e468324cb
unknown <garyshi0921@gmail.com>
2024-01-05 21:44:01 -0500

    add search bar and icon in nav bar

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GiFilmSpool } from "react-icons/gi";
 
 
 export default function Navbar() {
@@ -7,8 +8,16 @@ export default function Navbar() {
         <div className=" text-white max-w-screen-x1 mx-auto p-2  ">
           <div className="flex justify-between items-center">
           <img src="./logo-color.png" alt="" className='h-10 ' />
-           <ul>
-            <li>log in</li>
+           <ul className='flex items-center space-x-4'>
+            <li>
+              <button className='bg-dark-subtle p-1 rounded' >
+                <GiFilmSpool className='text-white ' size={24}/> 
+              </button> 
+            </li>
+            <li>
+              <input type="text" className='border-2 border-dark-subtle p-1 bg-transparent text-xl outline-none focus:border-white transition text-white' placeholder='"search...' />
+            </li>
+            <li className='text-white font-semibold text-lg'>Login</li>
            </ul>
 
           </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         secondary:'#272727',
         'dark-subtle':"rgba(255,255,255,0.5)",
         'light-subtle': "rgba(39,39,39,0.5)",
+        
       }
     },
   },
