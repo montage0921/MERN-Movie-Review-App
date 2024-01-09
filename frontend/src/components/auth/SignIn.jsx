aa2b4d0a7bf5494adc8623e9b12a4c5723b6ba7b
unknown <garyshi0921@gmail.com>
2024-01-08 21:50:16 -0500

    add password input for sign in form

--- frontend/src/components/auth/SignIn.jsx
+++ frontend/src/components/auth/SignIn.jsx
@@ -7,13 +7,19 @@ export default function SignIn() {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
       <Container>
-        <form className="bg-mzy-blue rounded p-6 w-72">
+        <form className="bg-mzy-blue rounded p-6 w-72 space-y-6">
           <Title>Sign in</Title>
           <FormInput
             name={"email"}
             placeholder={"john@email.com"}
             label={"Email"}
             type="input"
+          ></FormInput>
+          <FormInput
+            name={"password"}
+            placeholder={"*******"}
+            label={"Password"}
+            type="password"
           ></FormInput>
         </form>
       </Container>
