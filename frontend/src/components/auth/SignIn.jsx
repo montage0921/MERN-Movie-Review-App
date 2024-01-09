f22174da9d0baa49680bf58e7909e28201052017
unknown <garyshi0921@gmail.com>
2024-01-08 21:46:10 -0500

    refactor the code

--- frontend/src/components/auth/SignIn.jsx
+++ frontend/src/components/auth/SignIn.jsx
@@ -1,31 +1,20 @@
 import React from "react";
 import Container from "../Container";
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
 
 export default function SignIn() {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
       <Container>
         <form className="bg-mzy-blue rounded p-6 w-72">
-          <h1 className="textxl text-white font-semibold text-center">
-            Sign In
-          </h1>
-          <div className="flex flex-col-reverse">
-            <input
-              id="email"
-              type="text"
-              className="bg-transparent rounded border-2 border-dark-subtle 
-              w-full text-lg outline-none focus:p-1 text-white peer "
-              placeholder='"john@email.com'
-            />
-            <label
-              className="text-white font-semibold peer-focus:font-bold transition self-start"
-              htmlFor="email"
-            >
-              Email
-            </label>
-
-            <label htmlFor=""></label>
-          </div>
+          <Title>Sign in</Title>
+          <FormInput
+            name={"email"}
+            placeholder={"john@email.com"}
+            label={"Email"}
+            type="input"
+          ></FormInput>
         </form>
       </Container>
     </div>
---
+++ frontend/src/components/form/Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Title({ children }) {
+  return (
+    <h1 className="textxl text-white font-semibold text-center">{children}</h1>
+  );
+}
