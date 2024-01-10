5604e5a8bf3973fe0a590d368b1f854da9eae5eb
unknown <garyshi0921@gmail.com>
2024-01-09 22:37:46 -0500

    create forget-password form

--- frontend/src/components/CustomLink.jsx
+++ frontend/src/components/CustomLink.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function CustomLink({ to, children }) {
   return (
-    <Link className="text-white hover:text-dark-subtle transition" to={to}>
+    <Link
+      className="text-white text-sm hover:text-dark-subtle transition"
+      to={to}
+    >
       {children}
     </Link>
   );
--- frontend/src/components/auth/ForgetPassword.jsx
+++ frontend/src/components/auth/ForgetPassword.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import Container from "../Container";
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
+import Submit from "../form/Submit";
+import CustomLink from "../CustomLink";
 
 export default function ForgetPassword() {
-  return <div>ForgetPassword</div>;
+  return (
+    <div className="fixed  inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-mzy-blue rounded p-6 w-96 space-y-6 ">
+          <Title>Please Enter Your Email</Title>
+          <FormInput
+            name={"email"}
+            placeholder={"john@email.com"}
+            label={"Email"}
+            type="input"
+          ></FormInput>
+
+          <Submit value={"Send Verification Email"}></Submit>
+
+          <div className="flex justify-between">
+            <CustomLink to="/auth/signin">Sign In</CustomLink>
+            <CustomLink to="/auth/signup">Sign Up</CustomLink>
+          </div>
+        </form>
+      </Container>
+    </div>
+  );
 }
