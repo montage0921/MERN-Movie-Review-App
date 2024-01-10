09e63c5b6b75e391bd841434c8395a094397e44c
unknown <garyshi0921@gmail.com>
2024-01-09 23:34:36 -0500

    OTP number field will move to next after enter the nuber

--- frontend/src/components/auth/EmailVerification.jsx
+++ frontend/src/components/auth/EmailVerification.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Container from "../Container";
 import Title from "../form/Title";
 import Submit from "../form/Submit";
@@ -8,6 +8,20 @@ const OTP_LENGTH = 6;
 
 export default function EmailVerification() {
   const [otp, setOTP] = useState(new Array(OTP_LENGTH).fill(""));
+  const [activeOtpIndex, setActiveOtpIndex] = useState(0);
+  const inputRef = useRef();
+
+  const handleOtpChange = ({ target }, index) => {
+    const { value } = target;
+    const newOtp = [...otp];
+    newOtp[index] = value.substring(value.length - 1, value.length);
+    setOTP([...newOtp]);
+    setActiveOtpIndex(activeOtpIndex + 1);
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOtpIndex]);
 
   return (
     <div className="fixed  inset-0 bg-primary -z-10 flex justify-center items-center">
@@ -21,9 +35,13 @@ export default function EmailVerification() {
           </div>
 
           <div className="flex justify-center items-center space-x-4 ">
-            {otp.map((x) => {
+            {otp.map((_, index) => {
               return (
                 <input
+                  ref={activeOtpIndex === index ? inputRef : null}
+                  key={index}
+                  value={otp[index] || ""}
+                  onChange={(e) => handleOtpChange(e, index)}
                   className="w-12 h-12 border-2 rounded border-dark-subtle outline-none focus:border-white bg-transparent text-center text-xl text-white font-semibold"
                   type="number"
                 />
