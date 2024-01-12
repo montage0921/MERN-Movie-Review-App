7e2b9603e9672a956df2bca23f271e74cb46a741
unknown <garyshi0921@gmail.com>
2024-01-11 21:58:20 -0500

    finish otp verification keyboard operations

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Container from "../Container";
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
+import CustomLink from "../CustomLink";
+import Submit from "../form/Submit";
 
 export default function ConfirmPassword() {
-  return <div>ConfirmPassword</div>;
+  return (
+    <div className="fixed  inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-mzy-blue rounded p-6 w-72 space-y-6 ">
+          <Title>Password Confirmed</Title>
+        </form>
+      </Container>
+    </div>
+  );
 }
--- frontend/src/components/auth/EmailVerification.jsx
+++ frontend/src/components/auth/EmailVerification.jsx
@@ -6,37 +6,51 @@ import { useState } from "react";
 
 const OTP_LENGTH = 6;
 
+let currentOTPIndex;
+
 export default function EmailVerification() {
+  //update OTP array
   const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(""));
+  //update highlighted otp grid
   const [activeOtp, setActiveOtp] = useState(0);
   const inputRef = useRef();
 
-  function focusNextInputField(index) {
-    setActiveOtp(index + 1);
+  function focusNextInputField(currentOTPIndex) {
+    setActiveOtp(currentOTPIndex + 1);
   }
 
-  function focusPreInputField(index) {
-    setActiveOtp(index - 1);
+  function focusPreInputField(currentOTPIndex) {
+    setActiveOtp(currentOTPIndex - 1);
   }
 
-  function handleOnChange(e, index) {
+  function handleOnChange(e) {
+    console.log(currentOTPIndex);
     const value = e.target.value;
 
+    //only keep the last digit of user's input number
     const digit = value.substring(value.length - 1, value.length);
 
     const copyOtp = [...otp];
 
-    copyOtp[index] = digit;
+    copyOtp[currentOTPIndex] = digit;
 
-    console.log(value);
+    //go back to last OTP grid if user deletes current one
     if (!value) {
-      focusPreInputField(index);
+      focusPreInputField(currentOTPIndex);
     } else {
-      focusNextInputField(index);
+      focusNextInputField(currentOTPIndex);
     }
-
-    console.log(inputRef.current);
     setOtp(copyOtp);
+  }
+
+  //handle key board operations
+  function handleKeyDown(e, index) {
+    currentOTPIndex = index;
+    const key = e.key;
+
+    if (key === "Backspace") focusPreInputField(currentOTPIndex);
+    if (key === "ArrowLeft") focusPreInputField(currentOTPIndex);
+    if (key === "ArrowRight") focusNextInputField(currentOTPIndex);
   }
 
   useEffect(() => {
@@ -61,7 +75,8 @@ export default function EmailVerification() {
                   key={index}
                   value={otp[index]}
                   ref={activeOtp === index ? inputRef : null}
-                  onChange={(e) => handleOnChange(e, index)}
+                  onChange={handleOnChange}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
                   className="w-12 h-12 border-2 rounded border-dark-subtle outline-none focus:border-white bg-transparent text-center text-xl text-white font-semibold"
                   type="number"
                 />
