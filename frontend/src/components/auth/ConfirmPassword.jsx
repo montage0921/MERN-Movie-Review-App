f46c6474179ebb06bc3fb81daf32dde9cf782acb
unknown <garyshi0921@gmail.com>
2024-01-11 22:03:56 -0500

    Finish ConfirmPassword Component

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -9,8 +9,22 @@ export default function ConfirmPassword() {
   return (
     <div className="fixed  inset-0 bg-primary -z-10 flex justify-center items-center">
       <Container>
-        <form className="bg-mzy-blue rounded p-6 w-72 space-y-6 ">
-          <Title>Password Confirmed</Title>
+        <form className="bg-mzy-blue rounded p-6 w-96 space-y-6 ">
+          <Title>Please Confirm Your Email</Title>
+          <FormInput
+            name={"password"}
+            placeholder={"*******"}
+            label={"New Password"}
+            type="input"
+          ></FormInput>
+          <FormInput
+            name={"confirmPassword"}
+            placeholder={"********"}
+            label={"Confirm Password"}
+            type="input"
+          ></FormInput>
+
+          <Submit value={"Confirm Password"}></Submit>
         </form>
       </Container>
     </div>
