1741242caf1eb2677f1e28ab4d1707ae9d48156c
unknown <garyshi0921@gmail.com>
2024-01-15 23:21:58 -0500

    update sign up state

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../Container";
 import Title from "../form/Title";
 import FormInput from "../form/FormInput";
@@ -8,28 +8,57 @@ import { commonModalClasses } from "../../utils/theme";
 import FormContainer from "../form/FormContainer";
 
 export default function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = userInfo;
+
+  const handleOnChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    //a common way to update object type state
+    //this syntax create a shallow copy firstly
+    //then dymanically change a specific field in the object
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+  };
+
   return (
     <FormContainer>
       <Container>
-        <form className={commonModalClasses + " w-72"}>
+        <form onSubmit={handleSubmit} className={commonModalClasses + " w-72"}>
           <Title>Sign up</Title>
           <FormInput
             name={"name"}
             placeholder={"John Doe"}
             label={"Name"}
             type="input"
+            value={name}
+            onChange={handleOnChange}
           ></FormInput>
           <FormInput
             name={"email"}
             placeholder={"john@email.com"}
             label={"Email"}
             type="input"
+            value={email}
+            onChange={handleOnChange}
           ></FormInput>
           <FormInput
             name={"password"}
             placeholder={"*******"}
             label={"Password"}
             type="password"
+            value={password}
+            onChange={handleOnChange}
           ></FormInput>
 
           <Submit value={"Sign up"}></Submit>
